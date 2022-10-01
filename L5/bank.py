21784773676d90016bccc68bef5e7a5535f7fb4e
Fei <felicia.qf.chen@gmail.com>
2022-10-01 12:16:09 -0400

    run-time error, idk why

---
+++ L5/bank.py
@@ -0,0 +1,13 @@
+"""https://open.kattis.com/problems/bank"""
+N, T = map(int, input().split())
+q = [0 for _ in range(N)]                           # initialize queue of size N
+for _ in range(N):
+    cash, time = map(int, input().split())
+    if q[time] == 0:                                # if no one in that queue spot
+        q[time] = cash
+    else:
+        for minute in range(time, -1, -1):          # check for every min of the wait time 
+            if q[minute] < cash:                    # if there is anyone depositing less money
+                q[minute], cash = cash, q[minute]   # swap and check again
+            print(q)
+print(sum(q))
