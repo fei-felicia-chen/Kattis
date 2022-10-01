17df040fe464a9c171ec1404438fdc7d43701bae
Fei <felicia.qf.chen@gmail.com>
2022-10-01 10:52:31 -0400

    updated commercials

---
+++ L4/commercials.py
@@ -0,0 +1,8 @@
+n, cost = map(int, input().split())
+revenues = map(int, input().split())
+profits = [revenue - cost for revenue in revenues]
+
+for i in range(1, n):
+    curr = profits[i - 1] + profits[i]
+    if curr > profits[i]: profits[i] = curr
+print(max(profits))
