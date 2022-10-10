e66edee52f80ad9b70b6734e159fc2e669511507
Fei <felicia.qf.chen@gmail.com>
2022-10-10 09:54:56 -0400

    added comment

--- L5/bank.py
+++ L5/bank.py
@@ -1,4 +1,6 @@
 """https://open.kattis.com/problems/bank"""
+# This program gives and index out of range at a specific edge case, not sure why
+# See the Golang and/or Java versions.
 N, T = map(int, input().split())
 q = [0 for _ in range(N)]                           # initialize queue of size N
 for _ in range(N):
