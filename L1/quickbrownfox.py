552824ebbaf5aa899d36c748ded51ade82016da7
Fei <felicia.qf.chen@gmail.com>
2022-10-20 21:41:58 -0400

    made set

--- L1/quickbrownfox.py
+++ L1/quickbrownfox.py
@@ -2,6 +2,8 @@
 for i in range(int(input())):
     phrase = input().lower()
     missingLetters = []
-    for letter in "abcdefghijklmnopqrstuvwxyz":
+    alphabet = {'a','b','c','d','e','f','g','h','i','j','k','l','m',
+                'n','o','p','q','r','s','t','u','v','w','x','y','z'}
+    for letter in alphabet:
         if letter not in phrase: missingLetters += letter
     print("pangram") if not missingLetters else print("missing", ''.join(missingLetters))
