1393f2234a538e4f6d51bc8ca3a990a915bc416a
Fei <felicia.qf.chen@gmail.com>
2022-10-22 11:08:13 -0400

    reapeatingdecimal

---
+++ practicing/repeatingdecimal.cpp
@@ -0,0 +1,24 @@
+//https://open.kattis.com/problems/repeatingdecimal
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+
+    while (cin >> a >> b >> c) {
+        cout << "0.";
+        while (c--)
+        {
+            a *= 10;
+            long x = a / b;
+            cout << x;
+            a -= b * x;
+        }
+        cout << endl;
+    }
+    return 0;
+}
