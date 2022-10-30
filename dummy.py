cbf7be861a7f5621eb849ca0945e00a490c62aec
Fei <felicia.qf.chen@gmail.com>
2022-10-29 22:00:58 -0400

    ignore this lol

---
+++ dummy.py
@@ -0,0 +1,9 @@
+total_credits = 90
+credits_done = 27
+credits_per_semester = 12
+curr_credits = (13.5)*2
+credits_remaining = total_credits - credits_done
+
+nsemesters = (credits_remaining+curr_credits)/credits_per_semester
+
+print(nsemesters)
